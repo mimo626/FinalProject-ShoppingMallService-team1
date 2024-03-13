f863d8b20445af1ee0c19e59f12e981b27fc9151
mimo626 <yessuk5499@gmail.com>
2024-03-13 15:27:55 +0900

    Test

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
@@ -9,10 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("test1234", "강민주")
-        Log.d("test1234", "강민주2")
         Log.d("test1234", "채명훈")
-        Log.d("test1234", "채명훈2")
-        Log.d("test1234", "채명훈3")
-        Log.d("test1234", "채명훈4")
     }
 }
