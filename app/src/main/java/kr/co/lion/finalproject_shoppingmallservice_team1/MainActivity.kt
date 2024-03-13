e7487b11fb2951a7e1f1b29194cf1b12104b12b4
mimo626 <yessuk5499@gmail.com>
2024-03-13 14:31:33 +0900

    Test

---
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.lion.finalproject_shoppingmallservice_team1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("test1234", "민주")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FinalProject-ShoppingMallService_Team1"
+include(":app")
+ 
